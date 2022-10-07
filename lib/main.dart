138d6e88540208fa40c780fb8354592821b029d3
dduyen <duyendinh2021@outlook.com>
2022-10-07 11:01:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+// import 'package:untitled/facebook.dart';
+// import 'package:untitled/homepage.dart';
+
+import 'facebook.dart';
+import 'home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: FacebookLogin()
+        //FacebookLogin(),
+        );
+  }
+}
